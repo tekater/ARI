410c08fda9fa658e77d86cd5e0484a16940ce51d
tekater <120014627+tekater@users.noreply.github.com>
2023-01-23 19:22:21 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -28,9 +28,76 @@ public class Main {
         double score = 4.71 * ch / wo + 0.5 * wo / sent - 21.43;
         System.out.println("The score is: " + score);
 
-        //double action = scanner.nextDouble();
-        switch (score) {    // не знаю как сделать, пытается впихать Boolean
-            case (score > 5 & score < 6) :
+       //Integer action = Integer.parseInt(String.valueOf(score));
+        double action = Math.ceil(score);
+        // javac .\Main.java
+        // java Main
+        switch ((int)action) {    // Math.ceil(a) | Math.floor(a)
+            case 5:
+                System.out.println("This text should be understood by 5-6 year-olds." + " " + "For Kindergarten");
+                break;
+            case 6:
+                System.out.println("This text should be understood by 6-7 year-olds." + " " + "For First/Secind Grade");
+                break;
+            case 7:
+                System.out.println("This text should be understood by 7-9 year-olds." + " " + "For Third Grade");
+                break;
+            case 9:
+                System.out.println("This text should be understood by 9-10 year-olds." + " " + "For Fourth Grade");
+                break;
+            case 10:
+                System.out.println("This text should be understood by 10-11 year-olds." + " " + "For Fifth Grade");
+                break;
+            case 11:
+                System.out.println("This text should be understood by 11-12 year-olds." + " " + "For Sixth Grade");
+                break;
+            case 12:
+                System.out.println("This text should be understood by 12-13 year-olds." + " " + "For Seventh Grade");
+                break;
+            case 13:
+                System.out.println("This text should be understood by 13-14 year-olds." + " " + "For Eighth Grade");
+                break;
+            case 14:
+                System.out.println("This text should be understood by 14-15 year-olds." + " " + "For Ninth Grade");
+                break;
+            case 15:
+                System.out.println("This text should be understood by 15-16 year-olds." + " " + "For Tenth Grade");
+                break;
+            case 16:
+                System.out.println("This text should be understood by 16-17 year-olds." + " " + "For Eleventh Grade");
+                break;
+            case 17:
+                System.out.println("This text should be understood by 17-18 year-olds." + " " + "For Twelfth Grade");
+                break;
+            case 18:
+                System.out.println("This text should be understood by 18-24 year-olds." + " " + "For College Student");
+                break;
+            case 19:
+                System.out.println("This text should be understood by 18-24 year-olds." + " " +  "For College Student");
+                break;
+            case 20:
+                System.out.println("This text should be understood by 18-24 year-olds." +  " " + "For College Student");
+                break;
+            case 21:
+                System.out.println("This text should be understood by 18-24 year-olds." + " " +  "For College Student");
+                break;
+            case 22:
+                System.out.println("This text should be understood by 18-24 year-olds." + " " +  "For College Student");
+                break;
+            case 23:
+                System.out.println("This text should be understood by 18-24 year-olds." + " " +  "For College Student");
+                break;
+            case 24:
+                System.out.println("This text should be understood by 24+ year-olds." + " " +  "For Professor");
+                break;
+            default:
+                System.out.println("Not statement");
+        }
+    }
+}
+
+
+            /*case (score > 5 & score < 6):
                 System.out.println("Kindergarten");
                 break;
             case (score > 6 & score < 7):
@@ -73,7 +140,4 @@ public class Main {
                 System.out.println("Professor");
                 break;
             default:
-                System.out.println("Not");
-        }
-    }
-}
+                System.out.println("Not");*/
